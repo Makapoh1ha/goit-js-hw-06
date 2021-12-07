0a6fefd9342be1587ddbd90de0f95c9a7b93deff
Makapoh1ha <makapohiha@gmail.com>
2021-12-07 14:06:26 +0100

    all

---
+++ js/task-06.js
@@ -0,0 +1,14 @@
+const inputEl = document.querySelector('#validation-input');
+inputEl.addEventListener('blur', inputBlur);
+function inputBlur(event) {
+    const inputLength = event.currentTarget.value.length;
+    if (inputLength === Number(inputEl.dataset.length)) {
+        inputEl.classList.remove('invalid');
+        inputEl.classList.add('valid');
+
+    } else {
+        inputEl.classList.remove('valid');
+        inputEl.classList.add('invalid');
+
+    }
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,8 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const btn = document.querySelector('.change-color');
+btn.addEventListener('click', change_color);
+function change_color() {
+  document.body.style.background = getRandomHexColor();
+}
